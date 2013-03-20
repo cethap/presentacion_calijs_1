9609b5db9a2cfc9247a79af05e337654b42fb4ee
Cesar Tapasco <cethapgames@gmail.com>
2013-03-19 23:32:00 -0500

    novo con sockets

--- web.js
+++ web.js
@@ -1,12 +1,24 @@
-var express = require('express');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-var app = express.createServer(express.logger());
+app.listen(8000);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.on('ppevent', function (data) {
+    io.sockets.emit('news2', { hello: data.message });
+  });
 });
