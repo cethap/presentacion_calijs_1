ded692fc4c2da5c3457ae3b885f16ddad37c6acf
Cesar Tapasco <cesartapasco@cethap.local>
2013-09-09 00:13:24 -0500

    prueba

--- web.js
+++ web.js
@@ -1,24 +1,32 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+// var app = require('http').createServer(handler)
+//   , io = require('socket.io').listen(app)
+//   , fs = require('fs')
 
-app.listen(8000);
+// app.listen(8000);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+// function handler (req, res) {
+//   fs.readFile(__dirname + '/index.html',
+//   function (err, data) {
+//     if (err) {
+//       res.writeHead(500);
+//       return res.end('Error loading index.html');
+//     }
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+//     res.writeHead(200);
+//     res.end(data);
+//   });
+// }
 
-io.sockets.on('connection', function (socket) {
-  socket.on('ppevent', function (data) {
-    io.sockets.emit('news2', { hello: data.message });
-  });
-}); 
+// io.sockets.on('connection', function (socket) {
+//   socket.on('ppevent', function (data) {
+//     io.sockets.emit('news2', { hello: data.message });
+//   });
+// }); 
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
