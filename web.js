262dcead61c703b3f6aebdf3e6f5507ffb688b92
Cesar Tapasco <cesartapasco@cethap.local>
2013-09-09 00:42:47 -0500

    Nueva prueba

--- web.js
+++ web.js
@@ -1,32 +1,12 @@
-// var app = require('http').createServer(handler),
-//   io = require('socket.io').listen(app),
-//   fs = require('fs');
+var express = require('express');
 
-// app.listen(8000);
+var app = express.createServer(express.logger());
 
-// function handler (req, res) {
-//   fs.readFile(__dirname + '/index.html',
-//   function (err, data) {
-//     if (err) {
-//       res.writeHead(500);
-//       return res.end('Error loading index.html');
-//     }
+app.get('/', function(request, response) {
+  response.send('Este es el inicio de una gran app del camacho!');
+});
 
-//     res.writeHead(200);
-//     res.end(data);
-//   });
-// }
-
-// io.sockets.on('connection', function (socket) {
-//   socket.on('ppevent', function (data) {
-//     io.sockets.emit('news2', { hello: data.message });
-//   });
-// });
-
-
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Esta es una prueba de Heroku para ontar la app del proyecto en el camacho\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
