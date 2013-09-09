3294dae7043651cab786db34154bcfe5c185274a
Cesar Tapasco <cesartapasco@cethap.local>
2013-09-09 00:44:18 -0500

    Nueva prueba con sockets de nuevo

--- web.js
+++ web.js
@@ -1,12 +1,32 @@
-var express = require('express');
+// var app = require('http').createServer(handler)
+//   , io = require('socket.io').listen(app)
+//   , fs = require('fs')
 
-var app = express.createServer(express.logger());
+// app.listen(8000);
 
-app.get('/', function(request, response) {
-  response.send('Este es el inicio de una gran app del camacho!');
-});
+// function handler (req, res) {
+//   fs.readFile(__dirname + '/index.html',
+//   function (err, data) {
+//     if (err) {
+//       res.writeHead(500);
+//       return res.end('Error loading index.html');
+//     }
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//     res.writeHead(200);
+//     res.end(data);
+//   });
+// }
+
+// io.sockets.on('connection', function (socket) {
+//   socket.on('ppevent', function (data) {
+//     io.sockets.emit('news2', { hello: data.message });
+//   });
+// }); 
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
